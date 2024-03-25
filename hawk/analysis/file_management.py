58b4afddcf197e969e56af2643c8b77f9d4e7fe3
Teo Bucci <teobucci8@gmail.com>
2024-03-25 22:05:17 +0100

    Format file management util

--- hawk/analysis/file_management.py
+++ hawk/analysis/file_management.py
@@ -4,17 +4,17 @@ from typing import Any
 
 
 def save_to_pkl_file(target_file: str, data: Any, overwrite: bool = True) -> None:
-
     # Check if the file already exists
     if os.path.exists(target_file) and not overwrite:
         raise ValueError(f"File {target_file} already exists.")
-    
+
     # Create the directory and parent directories if they don't exist
     os.makedirs(os.path.dirname(target_file), exist_ok=True)
 
     # Save the data to the file
     with open(target_file, "wb") as f:
         pickle.dump(data, f)
+
 
 def load_from_pkl_file(source_file: str) -> Any:
     # Check if the file exists
@@ -24,5 +24,5 @@ def load_from_pkl_file(source_file: str) -> Any:
     # Load the data from the file
     with open(source_file, "rb") as f:
         data = pickle.load(f)
-    
+
     return data

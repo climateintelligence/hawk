17524793c69c09544fcb1fa0bb7f32b7b7a75cf5
Teo Bucci <teobucci8@gmail.com>
2024-03-25 23:11:01 +0100

    Add fake df and lint

--- hawk/processes/simulation_interactive.py
+++ hawk/processes/simulation_interactive.py
@@ -1,19 +1,35 @@
 import numpy as np
 import pandas as pd
 from birdy import WPSClient
-#from keras import models
 
+# from keras import models
+
+
+np.random.seed(0)
+n = 1000  # number of samples
+m = 15  # number of features
+
+data = {}
+for i in range(1, m + 1):
+    data[f"x{i}"] = np.random.normal(size=n)
+
+data["y"] = sum(data.values()) + np.random.normal(size=n)
+
+data = pd.DataFrame(data)
+
+n_test = int(0.20 * n)
+n_train = n - n_test
+data_test = data[n_train:]
+data = data[:n_train]
+
+data.head()
+
+target_name = "y"
 
 url = "http://localhost:5000/wps"
 wps = WPSClient(url, verify=False)
 help(wps)
 
-
-resp = wps.hello(name="Pluto")
-print(resp)
-resp.get()
-
-
-resp = wps.cyclone(start_day="2019-01-04", end_day="2019-01-06", area="Sindian")
+resp = wps.causal()
 print(resp)
 resp.get()
